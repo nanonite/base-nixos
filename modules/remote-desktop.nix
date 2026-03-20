4aba6f78f367f2c92f582eb6d51c224dc2902750
Roger <2349671+nanonite@users.noreply.github.com>
2026-03-19 22:45:21 -0500

    framework : laptop specific config and hardware

--- modules/remote-desktop.nix
+++ modules/remote-desktop.nix
@@ -66,7 +66,7 @@
   services.openssh = {
     enable = true;
     settings = {
-      PasswordAuthentication = false; # key-based auth only (more secure)
+      PasswordAuthentication = true; # key-based auth only (more secure)
       PermitRootLogin        = "no";
     };
   };
